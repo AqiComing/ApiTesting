5174ab3a8f6a10b09defb09cefd117fc6966de61
Nick Zhao <v-nizhao@microsoft.com>
2019-04-15 17:03:13 +0800

    log update

---
+++ log/logger.py
@@ -0,0 +1,17 @@
+import logging
+import json
+
+logging.basicConfig(level=logging.INFO,  # log level
+                    format='[%(asctime)s] %(levelname)s [%(funcName)s: %(filename)s, %(lineno)d] %(message)s',  # log格式
+                    datefmt='%Y-%m-%d %H:%M:%S',  # 日期格式
+                    filename='log.txt',  # 日志输出文件
+                    filemode='a')  # 追加模式
+
+def log_case_info(case_name,url,data,expect_res):
+    if isinstance(data,dict):
+        data=json.dumps(data,ensure_ascii=False)
+    logging.info('TestCase:{}'.format(case_name))
+    logging.info('Url :{}'.format(url))
+    logging.info('Data ：'+ data)
+    logging.info('Expect result :{}'.format(expect_res))
+    
--- test_user_login.py
+++ test_user_login.py
@@ -2,20 +2,28 @@ import unittest  # 导入unittest
 import json
 import requests
 from data.read_excel import get_test_data,excel_to_lists
-class TestUserLogin(unittest.TestCase):  # 类必须Test开头，继承TestCase才能识别为用例类
-    url = 'http://115.28.108.130:5000/api/user/login/'
+from log.logger import *
 
-    def test_user_login_normal(self):  # 一条测试用例，必须test_开头
-        data_list=excel_to_lists('TestUserLogin')
-        data=get_test_data(data_list,'test_user_login_normal')
-        # data = {"name": "张三", "password": "123456"}
+class TestUserLogin(unittest.TestCase):  # 类必须Test开头，继承TestCase才能识别为用例类   
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data_list =excel_to_lists('TestUserLogin')
+
+    # def setUp(self):
+    #     self.data_list =excel_to_lists('TestUserLogin')
+
+    def test_user_login_normal(self):  
+
+        data=get_test_data(self.data_list,'test_user_login_normal')
+        log_case_info('test_user_login_normal',data['url'],data['data'],data['expect_res'])
         res = requests.post(url=data['url'], data=json.loads(data['data']))
-        self.assertIn('登录成功', res.text)  # 断言
+        self.assertIn(res.text,data['expect_res'],'The text should be 登陆成功')  
         
     def test_user_login_password_wrong(self):
-        data = {"name": "张三", "password": "1234567"}
-        res = requests.post(url=self.url, data=data)
-        self.assertIn('用户名或密码错误', res.text)  # 断言
+        data=get_test_data(self.data_list,'test_user_login_password_wrong')
+        res = requests.post(url=data['url'], data=json.loads(data['data']))
+        self.assertIn(res.text,data['expect_res'],'The text should be 失败..')
 
 
 if __name__ == '__main__':  # 如果是直接从当前模块执行（非别的模块调用本模块）
